1466c8984636fcf56ed1e4a698192773af72afef
Dilshan Randika Sandaruwan <randikasandaruwan917@gmail.com>
2025-07-17 00:33:09 +0530

    Authentication for Admin almost done in backend

---
+++ controllers/authController.js
@@ -0,0 +1,55 @@
+// controllers/authController.js
+import Otp from "../models/Otp.js";
+import emailjs from "@emailjs/nodejs";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const requestOtp = async (req, res) => {
+  const { email } = req.body;
+
+  if (!process.env.ALLOWED_ADMINS.includes(email)) {
+    return res.status(401).json({ message: "Unauthorized Email" });
+  }
+
+  const otp = Math.floor(100000 + Math.random() * 900000).toString();
+
+  await Otp.create({ email, otp });
+
+  try {
+    await emailjs.send(
+      process.env.EMAILJS_SERVICE_ID,
+      process.env.EMAILJS_TEMPLATE_ID,
+      {
+        to_email: email,
+        otp_code: otp,
+        name: email === "dlshngamage917@gmail.com" ? "Dilshan" : "Admin",
+      },
+      {
+        publicKey: process.env.EMAILJS_PUBLIC_KEY,
+      }
+    );
+
+    res.json({ message: "OTP sent to email." });
+  } catch (err) {
+    console.error("EmailJS error:", err);
+    res.status(500).json({ message: "Failed to send OTP" });
+  }
+};
+
+export const verifyOtp = async (req, res) => {
+  const { email, otp } = req.body;
+
+  const existingOtp = await Otp.findOne({ email, otp });
+
+  if (!existingOtp) {
+    return res.status(401).json({ message: "Invalid or expired OTP" });
+  }
+
+  const token = jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+  await Otp.deleteMany({ email }); // cleanup
+
+  res.json({ token });
+};
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import packageRoute from './routes/packageRoute.js';
 import projectRoutes from './routes/projectRoutes.js';
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
@@ -20,7 +21,7 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => console.log('MongoDB connected'))
 .catch((err) => console.error(err));
 
-
+app.use("/api/auth", authRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api/packages', packageRoute); 
 
